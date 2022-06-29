f806cbd0f345f598177b6686c64ad32d7cf2402e
WilliamTreat <greedybirds@gmail.com>
2022-06-29 11:11:44 -0700

    all the js works, never saw tutorial instructions re home page buttons.

--- war.js
+++ war.js
@@ -1,5 +1,8 @@
 import Deck from "./deck.js"
 
+const coumputerCardSlot = document.querySelector(".computer-card-slot")
+
 const deck = new Deck()
 deck.shuffle()
-console.log(deck.cards)
+
+coumputerCardSlot.appendChild(deck.cards[0].getHTML())
